dc2e8836066b338575f0d2e3725a22ba4bbd85ce
Jeffrey <97353049+Delacrobix@users.noreply.github.com>
2023-07-18 17:14:53 -0500

    Change estilos del footer
    Add links en el footer

--- frontend/react-song-maker/src/modules/navigation/components/footer.js
+++ frontend/react-song-maker/src/modules/navigation/components/footer.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <footer>
-      <div className='footer-container'>
+    <footer className='footer'>
+      <section className='footer-container'>
         <div className='footer-content-container'>
-          <h3 className='website-logo'>Song Maker</h3>
-          <span className='footer-info'>111 111 1111</span>
-          <span className='footer-info'>correo@cotteo.con</span>
+          <div className='logo-container'>
+            <h3 className='website-logo'>Song Maker</h3>
+          </div>
         </div>
         <div className='footer-menus'>
           <div className='footer-content-container'>
@@ -16,27 +16,39 @@ const Footer = () => {
             <Link to='/home' className='menu-item-footer'>
               Home
             </Link>
-            <Link to='/#' className='menu-item-footer'>
-              option 1
+            <Link to='/create-song/tone' className='menu-item-footer'>
+              Create your own song
             </Link>
+            <Link to='/community' className='menu-item-footer'>
+              Community song
+            </Link>
+            <Link to='/about-me' className='menu-item-footer'>
+              About me
+            </Link>
+          </div>
+          <div className='footer-content-container'>
+            <span className='menu-title'>Help us</span>
             <Link to='/#' className='menu-item-footer'>
-              Option 2
+              Donations
+            </Link>
+            <Link
+              to='https://github.com/Delacrobix/Song-maker'
+              target='_blank'
+              className='menu-item-footer'
+            >
+              Repository
+            </Link>
+            <Link to='#' target='_blank' className='menu-item-footer'>
+              Report a bug
             </Link>
           </div>
           <div className='footer-content-container'>
             <span className='menu-title'>Legal</span>
             <Link to='/#' className='menu-item-footer'>
-              Option 1
-            </Link>
-            <Link to='/#' className='menu-item-footer'>
-              Option 2
-            </Link>
-            <Link to='/#' className='menu-item-footer'>
               Terms and conditions
             </Link>
           </div>
         </div>
-
         <div className='footer-content-container'>
           <span className='menu-title'>Follow me</span>
           <div className='social-container'>
@@ -57,7 +69,7 @@ const Footer = () => {
             ></Link>
           </div>
         </div>
-      </div>
+      </section>
       <div className='copyright-container'>
         <span className='copyright'>&copy;2023, jeffrm.com.co</span>
       </div>
