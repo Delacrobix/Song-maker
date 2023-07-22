77434f11733263b9a56825fce519fb7f025f58e2
Jeffrey <97353049+Delacrobix@users.noreply.github.com>
2023-07-22 13:00:40 -0500

    Cambiando estilos del circulo de quintas

--- frontend/react-song-maker/src/modules/songMaker/components/toneLayer.js
+++ frontend/react-song-maker/src/modules/songMaker/components/toneLayer.js
@@ -1,34 +1,34 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const ToneLayer = (props) => {
   const navigate = useNavigate();
   const { title } = props;
 
   function handleClick({ target: { innerHTML } }) {
-    navigate("/rhythm-selector", {
-      state: { tonality: innerHTML }
+    navigate('/rhythm-selector', {
+      state: { tonality: innerHTML },
     });
   }
 
   return (
-    <li className="radial-menu__menu-item">
-      <div className="radial-menu__menu-link-bg"></div>
-      <div className="radial-menu__menu-icon">
-        <span className="oi" title={title} aria-hidden="true"></span>
+    <li className='radial-menu__menu-item'>
+      <div className='radial-menu__menu-link-bg'></div>
+      <div className='radial-menu__menu-icon'>
+        <span className='oi' title={title} aria-hidden='true'>
+          {title}
+        </span>
       </div>
-      <div className="radial-menu__menu-content">
-        <div className="radial-menu__menu-content-wrapper">
-          <h6 className="radial-menu__menu-content-title">{title}</h6>
+      <div className='radial-menu__menu-content'>
+        <div className='radial-menu__menu-content-wrapper'>
+          <h6 className='radial-menu__menu-content-title'>{title}</h6>
         </div>
       </div>
       <p
-        className="radial-menu__menu-link"
-        id="select-tonality"
+        className='radial-menu__menu-link'
+        id='select-tonality'
         onClick={handleClick}
-      >
-        {title}
-      </p>
+      ></p>
     </li>
   );
 };
